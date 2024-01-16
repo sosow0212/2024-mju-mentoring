a9739ad4cd1d913d57a3742d0f72514d1c64431e
devholic22 <hyunjoon.tech@gmail.com>
2024-01-16 21:41:04 +0900

    feat: Board 엔티티 정의

---
+++ src/main/java/com/mju/mentoring/board/domain/Board.java
@@ -0,0 +1,40 @@
+package com.mju.mentoring.board.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private Board(final String title, final String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public static Board of(final String title, final String content) {
+        return new Board(title, content);
+    }
+
+    public void updateTitle(final String title) {
+        this.title = title;
+    }
+
+    public void updateContent(final String content) {
+        this.content = content;
+    }
+}
