9308e76f590b0958ff44cd09a4f49bbf7b5df980
zoid79 <zidwkd00@naver.com>
2024-01-17 20:22:05 +0900

    fin Member Table

---
+++ src/main/java/com/mju/mentoring/exam/board/domain/Member.java
@@ -0,0 +1,23 @@
+package com.mju.mentoring.exam.board.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String nickname;
+}
