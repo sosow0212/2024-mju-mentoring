176b9bab17b025751e34041499f4a0054daff366
devholic22 <hyunjoon.tech@gmail.com>
2024-01-18 15:34:11 +0900

    feat: BoardService 정의
    - CRUD 핵심 역할을 할 서비스 정의
    - 생성과 변경의 역할을 할 DTO들을 따로 생성, 레코드 (record) 사용

---
+++ src/main/java/com/mju/mentoring/board/service/BoardService.java
@@ -0,0 +1,43 @@
+package com.mju.mentoring.board.service;
+
+import com.mju.mentoring.board.domain.Board;
+import com.mju.mentoring.board.domain.BoardRepository;
+import com.mju.mentoring.board.service.dto.BoardCreateRequest;
+import com.mju.mentoring.board.service.dto.BoardUpdateRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    @Transactional
+    public Long save(final BoardCreateRequest request) {
+        Board newBoard = Board.of(request.title(), request.content());
+        return boardRepository.save(newBoard);
+    }
+
+    public Board find(final Long id) {
+        return boardRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException("해당 게시물이 없습니다."));
+    }
+
+    @Transactional
+    public Board update(final Long id, final BoardUpdateRequest request) {
+        Board targetBoard = find(id);
+
+        targetBoard.updateTitle(request.title());
+        targetBoard.updateContent(request.content());
+
+        return targetBoard;
+    }
+
+    @Transactional
+    public void delete(final Long id) {
+        boardRepository.deleteById(id);
+    }
+}
