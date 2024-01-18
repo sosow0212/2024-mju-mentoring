d9897aa2dabce0dbc363c7044b14822f2a34f60c
WonyuChoi <phillip07290@naver.com>
2024-01-18 19:45:41 +0900

    feat: board CRUD 메서드

---
+++ src/main/java/com/mju/mentoring/board/controller/BoardController.java
@@ -0,0 +1,44 @@
+package com.mju.mentoring.board.controller;
+
+import com.mju.mentoring.board.controller.dto.BoardResponse;
+import com.mju.mentoring.board.service.dto.BoardCreateRequest;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/boards")
+public class BoardController {
+
+    @PostMapping
+    public ResponseEntity<Void> save(@RequestBody final BoardCreateRequest boardCreateRequest) {
+
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BoardResponse>> findAll() {
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BoardResponse> findBoardById(@PathVariable final Long id) {
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateById(@PathVariable final Long id) {
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable final Long id) {
+
+    }
+}
