3e288b93f902b824d3f69549306803f4ce62e0a1
WonyuChoi <phillip07290@naver.com>
2024-01-25 19:16:48 +0900

    refactor: 누락된 final 키워드 추가

--- src/main/java/com/mju/mentoring/board/domain/BoardRepository.java
+++ src/main/java/com/mju/mentoring/board/domain/BoardRepository.java
@@ -5,11 +5,11 @@ import java.util.Optional;
 
 public interface BoardRepository {
 
-    Board save(Board board);
+    Board save(final Board board);
 
     List<Board> findAll();
 
-    Optional<Board> findById(Long id);
+    Optional<Board> findById(final Long id);
 
-    void deleteById(Long id);
+    void deleteById(final Long id);
 }
