0de5f3d6fc4e00d95a2418acc012298ebb74353a
WonyuChoi <phillip07290@naver.com>
2024-01-25 18:36:12 +0900

    refactor: member의 인증 정보를 VO로 분리

--- src/main/java/com/mju/mentoring/member/domain/Member.java
+++ src/main/java/com/mju/mentoring/member/domain/Member.java
@@ -1,22 +1,12 @@
 package com.mju.mentoring.member.domain;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Entity
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder
-@Getter
 public class Member {
 
     @Id
@@ -25,9 +15,11 @@ public class Member {
 
     @Embedded
     AuthInformation authInformation;
-
-    @Column(unique = true, nullable = false)
     String nickname;
+
+    protected Member() {
+
+    }
 
     private Member(final AuthInformation authInformation, final String nickname) {
         this.authInformation = authInformation;
@@ -36,13 +28,5 @@ public class Member {
 
     public static Member of(final String username, final String password, final String nickname) {
         return new Member(AuthInformation.of(username, password), nickname);
-    }
-
-    public boolean isValidPassword(final String password) {
-        return authInformation.isValidPassword(password);
-    }
-
-    public String getUsername() {
-        return authInformation.getUsername();
     }
 }
