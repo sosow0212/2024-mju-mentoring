d7810a852381c38eed9268fbc082fbd7abb4dee3
WonyuChoi <phillip07290@naver.com>
2024-01-25 19:07:53 +0900

    refactor: dto record로 리팩토링

--- src/main/java/com/mju/mentoring/board/application/BoardService.java
+++ src/main/java/com/mju/mentoring/board/application/BoardService.java
@@ -1,5 +1,6 @@
 package com.mju.mentoring.board.application;
 
+import com.mju.mentoring.board.application.dto.BoardUpdateRequest;
 import com.mju.mentoring.board.domain.Board;
 import com.mju.mentoring.board.domain.BoardRepository;
 import com.mju.mentoring.board.application.dto.BoardCreateRequest;
@@ -17,7 +18,7 @@ public class BoardService {
 
     @Transactional
     public Long save(final BoardCreateRequest boardCreateRequest) {
-        Board board = Board.of(boardCreateRequest.getTitle(), boardCreateRequest.getContent());
+        Board board = Board.of(boardCreateRequest.title(), boardCreateRequest.content());
         Board savedBoard = boardRepository.save(board);
 
         return savedBoard.getId();
@@ -35,7 +36,7 @@ public class BoardService {
     @Transactional
     public void update(final Long id, final BoardUpdateRequest boardUpdateRequest) {
         this.findById(id)
-            .update(boardUpdateRequest.getTitle(), boardUpdateRequest.getContent());
+            .update(boardUpdateRequest.title(), boardUpdateRequest.content());
     }
 
     @Transactional
