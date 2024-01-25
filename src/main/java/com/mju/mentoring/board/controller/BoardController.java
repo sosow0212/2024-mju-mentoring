dd68a3fa02c9cd80a73f4f846ad9888e9e484b83
devholic22 <hyunjoon.tech@gmail.com>
2024-01-25 16:34:07 +0900

    refactor: PutMapping을 PatchMapping으로 수정
    - Restful 원칙에 따르기 위해 PutMapping 대신 PatchMapping으로 수정

--- src/main/java/com/mju/mentoring/board/controller/BoardController.java
+++ src/main/java/com/mju/mentoring/board/controller/BoardController.java
@@ -11,9 +11,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,7 +49,7 @@ public class BoardController {
         return ResponseEntity.ok(response);
     }
 
-    @PutMapping("/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<BoardUpdateResponse> updateText(@PathVariable final Long id,
                                                           @RequestBody final BoardTextUpdateRequest request) {
         boardService.updateText(id, request);
