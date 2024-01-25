a715a464ef4d49baa1d2ff6430397b21a8175cbf
devholic22 <hyunjoon.tech@gmail.com>
2024-01-25 18:09:49 +0900

    test: BoardTest 작성
    - 글의 제목, 내용 수정 테스트

---
+++ src/test/java/com/mju/mentoring/board/domain/BoardTest.java
@@ -0,0 +1,32 @@
+package com.mju.mentoring.board.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class BoardTest {
+
+    @Test
+    void 글의_제목과_내용을_수정한다() {
+        // given
+        String originTitle = "title";
+        String originContent = "content";
+        String editTitle = "title (edited)";
+        String editContent = "content (edited)";
+        Board originBoard = new Board(originTitle, originContent);
+
+        // when
+        originBoard.updateText(editTitle, editContent);
+
+        // then
+        assertSoftly((softly) -> {
+            assertThat(originBoard.getTitle()).isEqualTo(editTitle);
+            assertThat(originBoard.getContent()).isEqualTo(editContent);
+        });
+    }
+}
