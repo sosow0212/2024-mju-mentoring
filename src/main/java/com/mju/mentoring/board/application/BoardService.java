99ec2b70e2745e1e8eb0b86c3ab5622941fa41fc
WonyuChoi <phillip07290@naver.com>
2024-01-25 18:29:40 +0900

    refactor: 정적 팩토리 메서드로 board를 생성하도록 수정

--- src/main/java/com/mju/mentoring/board/application/BoardService.java
+++ src/main/java/com/mju/mentoring/board/application/BoardService.java
@@ -1,11 +1,8 @@
 package com.mju.mentoring.board.application;
 
-import com.mju.mentoring.board.application.dto.BoardDeleteRequest;
-import com.mju.mentoring.board.application.dto.BoardUpdateRequest;
 import com.mju.mentoring.board.domain.Board;
 import com.mju.mentoring.board.domain.BoardRepository;
 import com.mju.mentoring.board.application.dto.BoardCreateRequest;
-import com.mju.mentoring.board.exception.exceptions.BoardNotFoundException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,7 +17,7 @@ public class BoardService {
 
     @Transactional
     public Long save(final BoardCreateRequest boardCreateRequest) {
-        Board board = Board.of(boardCreateRequest.title(), boardCreateRequest.content());
+        Board board = Board.of(boardCreateRequest.getTitle(), boardCreateRequest.getContent());
         Board savedBoard = boardRepository.save(board);
 
         return savedBoard.getId();
@@ -32,23 +29,17 @@ public class BoardService {
 
     public Board findById(final Long id) {
         return boardRepository.findById(id)
-            .orElseThrow(BoardNotFoundException::new);
+            .orElseThrow(IllegalArgumentException::new);
     }
 
     @Transactional
     public void update(final Long id, final BoardUpdateRequest boardUpdateRequest) {
         this.findById(id)
-            .update(boardUpdateRequest.title(), boardUpdateRequest.content());
+            .update(boardUpdateRequest.getTitle(), boardUpdateRequest.getContent());
     }
 
     @Transactional
     public void deleteById(final Long id) {
-        Board board = findById(id);
-        boardRepository.delete(board);
-    }
-
-    @Transactional
-    public void deleteAllById(final BoardDeleteRequest deleteRequest) {
-        boardRepository.deleteAllById(deleteRequest.ids());
+        boardRepository.deleteById(id);
     }
 }
