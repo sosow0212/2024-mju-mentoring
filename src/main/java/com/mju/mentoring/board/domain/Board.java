e7225ae53f49e08a5b37f8333a719e237c338669
WonyuChoi <phillip07290@naver.com>
2024-01-27 03:08:46 +0900

    feat: board 객체에 builder 추가

--- src/main/java/com/mju/mentoring/board/domain/Board.java
+++ src/main/java/com/mju/mentoring/board/domain/Board.java
@@ -5,8 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 
 @Entity
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class Board {
 
     @Id
