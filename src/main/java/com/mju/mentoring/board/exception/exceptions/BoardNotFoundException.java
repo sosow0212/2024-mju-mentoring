6459e1693b262bbbf63a3a838e5672d3776e888c
WonyuChoi <phillip07290@naver.com>
2024-01-26 17:30:45 +0900

    feat: board를 찾지 못했을 때의 예외 객체

---
+++ src/main/java/com/mju/mentoring/board/exception/exceptions/BoardNotFoundException.java
@@ -0,0 +1,9 @@
+package com.mju.mentoring.board.exception.exceptions;
+
+public class BoardNotFoundException extends RuntimeException {
+
+    public BoardNotFoundException() {
+        super("해당 게시물을 찾을 수 없습니다");
+    }
+
+}
