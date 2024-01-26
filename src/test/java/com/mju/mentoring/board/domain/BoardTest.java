8dd9439841fcc0f10370858d7deeea6be79910be
WonyuChoi <phillip07290@naver.com>
2024-01-27 03:10:29 +0900

    test: Board 도메인 객체 테스트

--- src/test/java/com/mju/mentoring/board/domain/BoardTest.java
+++ src/test/java/com/mju/mentoring/board/domain/BoardTest.java
@@ -1,7 +1,7 @@
 package com.mju.mentoring.board.domain;
 
 import static com.mju.mentoring.board.fixture.BoardFixture.id_없는_게시글_생성;
-import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -12,19 +12,15 @@ import org.junit.jupiter.api.Test;
 class BoardTest {
 
     @Test
-    public void 게시글_업데이트() {
+    public void 게시글_업데이트_값_객체_인스턴스_교체() {
         // given
         Board board = id_없는_게시글_생성();
-        String updateTitle = "업데이트 제목";
-        String updateContent = "업데이트 내용";
+        Description origin = board.getDescription();
 
         // when
-        board.update(updateTitle, updateContent);
+        board.update("업데이트 제목", "업데이트 내용");
 
         // then
-        assertSoftly(softly -> {
-            softly.assertThat(board.getTitle()).isEqualTo(updateTitle);
-            softly.assertThat(board.getContent()).isEqualTo(updateContent);
-        });
+        assertNotEquals(origin, board.getDescription());
     }
 }
