198f711b4954411e9bf485f773100365334b7a33
WonyuChoi <phillip07290@naver.com>
2024-01-27 03:11:41 +0900

    test: 테스트를 위한 가짜 repository 구현 객체

---
+++ src/test/java/com/mju/mentoring/board/infrastructure/BoardFakeRepository.java
@@ -0,0 +1,51 @@
+package com.mju.mentoring.board.infrastructure;
+
+import com.mju.mentoring.board.domain.Board;
+import com.mju.mentoring.board.domain.BoardRepository;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+public class BoardFakeRepository implements BoardRepository {
+
+    private final Map<Long, Board> db = new HashMap<>();
+    private Long id = 1L;
+
+    @Override
+    public Board save(final Board board) {
+        Board savedBoard = Board.builder()
+            .id(id)
+            .description(board.getDescription())
+            .build();
+
+        db.put(id++, savedBoard);
+        return savedBoard;
+    }
+
+    @Override
+    public List<Board> findAll() {
+        return new ArrayList<>(db.values());
+    }
+
+    @Override
+    public Optional<Board> findById(final Long id) {
+        return db.values()
+            .stream()
+            .filter(board -> board.getId().equals(id))
+            .findAny();
+    }
+
+    @Override
+    public void deleteById(final Long id) {
+        if (!db.containsKey(id)) {
+            return;
+        }
+        db.remove(id);
+    }
+}
