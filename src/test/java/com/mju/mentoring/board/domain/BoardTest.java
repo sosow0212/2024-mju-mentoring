294106a875593b86140e2224afa1d63d77d7a360
WonyuChoi <phillip07290@naver.com>
2024-01-31 15:50:08 +0900

    test: 게시물 업데이트 후 수정 되었는지 확인

--- src/test/java/com/mju/mentoring/board/domain/BoardTest.java
+++ src/test/java/com/mju/mentoring/board/domain/BoardTest.java
@@ -1,7 +1,7 @@
 package com.mju.mentoring.board.domain;
 
 import static com.mju.mentoring.board.fixture.BoardFixture.id_없는_게시글_생성;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -12,15 +12,19 @@ import org.junit.jupiter.api.Test;
 class BoardTest {
 
     @Test
-    public void 게시글_업데이트_값_객체_인스턴스_교체() {
+    public void 게시글_업데이트() {
         // given
         Board board = id_없는_게시글_생성();
-        Description origin = board.getDescription();
+        String updateTitle = "업데이트 제목";
+        String updateContent = "업데이트 내용";
 
         // when
-        board.update("업데이트 제목", "업데이트 내용");
+        board.update(updateTitle, updateContent);
 
         // then
-        assertNotEquals(origin, board.getDescription());
+        assertSoftly(softly -> {
+            softly.assertThat(board.getTitle()).isEqualTo(updateTitle);
+            softly.assertThat(board.getContent()).isEqualTo(updateContent);
+        });
     }
 }
