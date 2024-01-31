73d481424702a52d713df06e0a417c25828d80f9
WonyuChoi <phillip07290@naver.com>
2024-02-01 02:45:01 +0900

    test: 업데이트 로직 테스트

--- src/test/java/com/mju/mentoring/board/application/BoardServiceTest.java
+++ src/test/java/com/mju/mentoring/board/application/BoardServiceTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import com.mju.mentoring.board.application.dto.BoardCreateRequest;
+import com.mju.mentoring.board.application.dto.BoardUpdateRequest;
 import com.mju.mentoring.board.domain.Board;
 import com.mju.mentoring.board.domain.BoardRepository;
 import com.mju.mentoring.board.exception.exceptions.BoardNotFoundException;
@@ -40,8 +41,8 @@ class BoardServiceTest {
 
         // then
         assertSoftly(softly -> {
-            softly.assertThat(findBoards.size())
-                .isEqualTo(1);
+            softly.assertThat(findBoards)
+                .hasSize(1);
 
             softly.assertThat(savedId)
                 .isEqualTo(1L);
@@ -86,6 +87,40 @@ class BoardServiceTest {
     void id로_조회한_게시글이_없을_시_예외처리() {
         // when & then
         assertThatThrownBy(() -> boardService.findById(0L))
+            .isInstanceOf(BoardNotFoundException.class);
+    }
+
+    @Test
+    void 게시물_업데이트() {
+        // given
+        String originTitle = "origin title";
+        String originContent = "origin content";
+        BoardCreateRequest createRequest = new BoardCreateRequest(originTitle, originContent);
+
+        String updateTitle = "update title";
+        String updateContent = "update content";
+        BoardUpdateRequest updateRequest = new BoardUpdateRequest(updateTitle, updateContent);
+
+        // when
+        Long savedId = boardService.save(createRequest);
+        boardService.update(savedId, updateRequest);
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(updateTitle).isEqualTo(updateTitle);
+            softly.assertThat(updateContent).isEqualTo(updateContent);
+        });
+    }
+
+    @Test
+    void 없는_게시물을_업데이트_시_예외처리() {
+        // given
+        String updateTitle = "update title";
+        String updateContent = "update content";
+        BoardUpdateRequest updateRequest = new BoardUpdateRequest(updateTitle, updateContent);
+
+        // when & then
+        assertThatThrownBy(() -> boardService.update(0L, updateRequest))
             .isInstanceOf(BoardNotFoundException.class);
     }
 }
