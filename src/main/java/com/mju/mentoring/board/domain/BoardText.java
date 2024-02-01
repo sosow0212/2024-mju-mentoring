1305e02398cfbb916d3e8c081f034e4f817e9958
devholic22 <hyunjoon.tech@gmail.com>
2024-02-01 10:57:04 +0900

    refactor: EqualsAndHashCode 적용
    - BoardText EqualsAndHashCode 적용

--- src/main/java/com/mju/mentoring/board/domain/BoardText.java
+++ src/main/java/com/mju/mentoring/board/domain/BoardText.java
@@ -4,10 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@EqualsAndHashCode
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Embeddable
