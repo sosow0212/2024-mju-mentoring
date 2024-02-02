b09495ff4a3fc53e222250dd6ebb1257d15a29d5
WonyuChoi <phillip07290@naver.com>
2024-02-02 23:20:50 +0900

    feat: Mock Bean 캐시를 위한 Base 추상 클래스 생성

--- src/test/java/com/mju/mentoring/board/ui/BoardControllerWebMvcTest.java
+++ src/test/java/com/mju/mentoring/board/ui/BoardControllerWebMvcTest.java
@@ -8,37 +8,16 @@ import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuild
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mju.mentoring.board.application.BoardService;
 import com.mju.mentoring.board.application.dto.BoardCreateRequest;
 import com.mju.mentoring.board.application.dto.BoardDeleteRequest;
 import com.mju.mentoring.board.application.dto.BoardUpdateRequest;
 import com.mju.mentoring.board.domain.Board;
+import com.mju.mentoring.global.BaseControllerWebMvcTest;
 import java.util.List;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-@SuppressWarnings("NonAsciiCharacters")
-@MockBean(JpaMetamodelMappingContext.class)
-@WebMvcTest(BoardController.class)
-class BoardControllerWebMvcTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @MockBean
-    private BoardService boardService;
+class BoardControllerWebMvcTest extends BaseControllerWebMvcTest {
 
     @Test
     void 게시글_저장() throws Exception {
--- src/test/java/com/mju/mentoring/global/BaseControllerWebMvcTest.java
+++ src/test/java/com/mju/mentoring/global/BaseControllerWebMvcTest.java
@@ -1,24 +1,28 @@
 package com.mju.mentoring.global;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mju.mentoring.board.application.BoardService;
 import com.mju.mentoring.board.ui.BoardController;
-import com.mju.mentoring.member.application.auth.AuthService;
-import com.mju.mentoring.member.ui.auth.AuthController;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.test.web.servlet.MockMvc;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
 @MockBean(JpaMetamodelMappingContext.class)
-@WebMvcTest({BoardController.class, AuthController.class})
+@WebMvcTest({BoardController.class})
 public abstract class BaseControllerWebMvcTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
 
     @MockBean
     protected BoardService boardService;
-
-    @MockBean
-    protected AuthService authService;
 }
