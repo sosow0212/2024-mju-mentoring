45f940c979d9b8405fd0a98ad0c7ffc89aaaaaf2
WonyuChoi <phillip07290@naver.com>
2024-02-04 19:38:42 +0900

    test: 인수 테스트에 필요한 기능 먼저 작성

--- src/test/java/com/mju/mentoring/member/ui/MemberAcceptanceTest.java
+++ src/test/java/com/mju/mentoring/member/ui/MemberAcceptanceTest.java
@@ -1,8 +1,5 @@
 package com.mju.mentoring.member.ui;
 
-import static com.mju.mentoring.member.fixture.MemberFixture.멤버_생성;
-
-import com.mju.mentoring.member.domain.Member;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -11,16 +8,13 @@ import org.junit.jupiter.api.Test;
 @SuppressWarnings("NonAsciiCharacters")
 public class MemberAcceptanceTest extends MemberAcceptanceTestFixture {
 
-    private static final String 회원가입_url = "/auth/signup";
-    private static final String 로그인_url = "/auth/signin";
-
     @Test
     void 회원가입_테스트() {
         // given
         var 회원가입_요청서 = 회원가입_요청();
 
         // when
-        var 회원가입_결과 = 회원가입(회원가입_요청서, 회원가입_url);
+        var 회원가입_결과 = 회원가입(회원가입_요청서);
 
         // then
         회원가입_검증(회원가입_결과);
@@ -29,12 +23,10 @@ public class MemberAcceptanceTest extends MemberAcceptanceTestFixture {
     @Test
     void 로그인_테스트() {
         // given
-        Member 멤버 = 멤버_생성();
-        로그인할_멤버_저장(멤버);
         var 로그인_요청서 = 로그인_요청();
 
         // when
-        var 로그인_결과 = 로그인(로그인_요청서, 로그인_url);
+        var 로그인_결과 = 로그인(로그인_요청서);
 
         // then
         로그인_검증(로그인_결과);
--- src/test/java/com/mju/mentoring/member/ui/MemberAcceptanceTestFixture.java
+++ src/test/java/com/mju/mentoring/member/ui/MemberAcceptanceTestFixture.java
@@ -1,77 +1,11 @@
 package com.mju.mentoring.member.ui;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.SoftAssertions.assertSoftly;
-
 import com.mju.mentoring.global.BaseAcceptanceTest;
-import com.mju.mentoring.member.application.auth.dto.SignInRequest;
-import com.mju.mentoring.member.application.auth.dto.SignupRequest;
-import com.mju.mentoring.member.domain.Member;
-import com.mju.mentoring.member.domain.MemberRepository;
-import com.mju.mentoring.member.ui.auth.dto.TokenResponse;
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.response.ExtractableResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
 public class MemberAcceptanceTestFixture extends BaseAcceptanceTest {
 
-    private static final String MEMBER_DEFAULT_USERNAME = "id";
-    private static final String MEMBER_DEFAULT_NICKNAME = "nickname";
-    private static final String MEMBER_DEFAULT_PASSWORD = "password";
-
-    @Autowired
-    MemberRepository memberRepository;
-
-    protected void 로그인할_멤버_저장(final Member member) {
-        memberRepository.save(member);
-    }
-
-    protected SignupRequest 회원가입_요청() {
-        return new SignupRequest(MEMBER_DEFAULT_USERNAME, MEMBER_DEFAULT_NICKNAME,
-            MEMBER_DEFAULT_PASSWORD);
-    }
-
-    protected ExtractableResponse 회원가입(final SignupRequest request, final String url) {
-        return RestAssured.given().log().all()
-            .given()
-            .body(request)
-            .contentType(ContentType.JSON)
-            .when()
-            .post(url)
-            .then()
-            .extract();
-    }
-
-    protected void 회원가입_검증(ExtractableResponse response) {
-        int code = response.statusCode();
-
-        assertThat(code).isEqualTo(HttpStatus.CREATED.value());
-    }
-
-    protected SignInRequest 로그인_요청() {
-        return new SignInRequest(MEMBER_DEFAULT_USERNAME, MEMBER_DEFAULT_PASSWORD);
-    }
-
-    protected ExtractableResponse 로그인(final SignInRequest request, final String url) {
-        return RestAssured.given().log().all()
-            .given()
-            .body(request)
-            .contentType(ContentType.JSON)
-            .when()
-            .post(url)
-            .then()
-            .extract();
-    }
-
-    protected void 로그인_검증(ExtractableResponse response) {
-        int code = response.statusCode();
-        TokenResponse tokenResponse = response.as(TokenResponse.class);
-
-        assertSoftly(softly -> {
-            softly.assertThat(code).isEqualTo(HttpStatus.OK.value());
-            softly.assertThat(tokenResponse).isNotNull();
-        });
-    }
 }
