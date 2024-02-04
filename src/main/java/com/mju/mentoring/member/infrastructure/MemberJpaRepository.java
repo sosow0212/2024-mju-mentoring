d7ba3fcc74c207a76f83c3c8647be70b719da672
WonyuChoi <phillip07290@naver.com>
2024-02-04 20:23:40 +0900

    feat: jpa repository 객체

--- src/main/java/com/mju/mentoring/member/infrastructure/MemberJpaRepository.java
+++ src/main/java/com/mju/mentoring/member/infrastructure/MemberJpaRepository.java
@@ -1,16 +1,13 @@
 package com.mju.mentoring.member.infrastructure;
 
 import com.mju.mentoring.member.domain.Member;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberJpaRepository extends JpaRepository<Member, Long> {
+public interface MemberJpaRepository extends JpaRepository<Long, Member> {
 
-    Member save(final Member member);
+    void save(final Member member);
 
-    boolean existsByAuthInformationUsername(final String username);
+    boolean isExistByUsername(final String username);
 
-    boolean existsByNickname(final String nickname);
-
-    Optional<Member> findMemberByAuthInformationUsername(final String username);
+    boolean isExistByNickname(final String nickname);
 }
