f307f191daa5fbd3a3093b15cc528835e5bb4bb8
WonyuChoi <phillip07290@naver.com>
2024-02-04 20:23:19 +0900

    feat: member 레포지토리 역할 지정

--- src/main/java/com/mju/mentoring/member/domain/MemberRepository.java
+++ src/main/java/com/mju/mentoring/member/domain/MemberRepository.java
@@ -1,14 +1,10 @@
 package com.mju.mentoring.member.domain;
 
-import java.util.Optional;
-
 public interface MemberRepository {
 
     void save(final Member member);
 
-    boolean existsByUsername(final String username);
+    boolean isExistByUsername(final String username);
 
-    boolean existsByNickname(final String nickname);
-
-    Optional<Member> findByUsername(final String username);
+    boolean isExistByNickname(final String nickname);
 }
