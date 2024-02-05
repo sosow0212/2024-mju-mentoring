6f1ec0b668660fc037ef959dccadabae8574024a
WonyuChoi <phillip07290@naver.com>
2024-02-05 20:20:36 +0900

    feat: password 비교 기능 추가

--- src/main/java/com/mju/mentoring/member/domain/AuthInformation.java
+++ src/main/java/com/mju/mentoring/member/domain/AuthInformation.java
@@ -2,9 +2,17 @@ package com.mju.mentoring.member.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import java.util.Objects;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@EqualsAndHashCode
 public class AuthInformation {
 
     @Column(unique = true, nullable = false)
@@ -13,42 +21,11 @@ public class AuthInformation {
     @Column(nullable = false)
     private String password;
 
-    protected AuthInformation() {
-
-    }
-
-    private AuthInformation(final String username, final String password) {
-        this.username = username;
-        this.password = password;
-    }
-
     public static AuthInformation of(final String username, final String password) {
         return new AuthInformation(username, password);
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public boolean equals(final Object object) {
-        if (this == object) {
-            return true;
-        }
-        if (object == null || getClass() != object.getClass()) {
-            return false;
-        }
-        AuthInformation that = (AuthInformation) object;
-        return Objects.equals(username, that.username) && Objects.equals(password,
-            that.password);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(username, password);
+    public boolean isValidPassword(final String password) {
+        return this.password.equals(password);
     }
 }
