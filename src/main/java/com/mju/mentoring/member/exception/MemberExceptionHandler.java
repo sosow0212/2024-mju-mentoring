bc46535a73e989fee49ac970f08acaed48f01dd4
WonyuChoi <phillip07290@naver.com>
2024-02-05 20:26:21 +0900

    feat: member 도메인 예외 핸들러

--- src/main/java/com/mju/mentoring/member/exception/MemberExceptionHandler.java
+++ src/main/java/com/mju/mentoring/member/exception/MemberExceptionHandler.java
@@ -1,5 +1,9 @@
-package com.mju.mentoring.member.exception.exceptions;
+package com.mju.mentoring.member.exception;
 
+import com.mju.mentoring.member.exception.exceptions.DuplicateNicknameException;
+import com.mju.mentoring.member.exception.exceptions.DuplicateUsernameException;
+import com.mju.mentoring.member.exception.exceptions.MemberNotFoundException;
+import com.mju.mentoring.member.exception.exceptions.WrongPasswordException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,6 +22,18 @@ public class MemberExceptionHandler {
     public ResponseEntity<String> handleDuplicateNicknameException(
         final DuplicateNicknameException exception) {
         return getResponseWithStatus(HttpStatus.CONFLICT, exception);
+    }
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    public ResponseEntity<String> handleMemberNotFoundException(
+        final DuplicateNicknameException exception) {
+        return getResponseWithStatus(HttpStatus.NOT_FOUND, exception);
+    }
+
+    @ExceptionHandler(WrongPasswordException.class)
+    public ResponseEntity<String> handleWrongPasswordException(
+        final DuplicateNicknameException exception) {
+        return getResponseWithStatus(HttpStatus.BAD_REQUEST, exception);
     }
 
     private ResponseEntity<String> getResponseWithStatus(final HttpStatus httpStatus,
