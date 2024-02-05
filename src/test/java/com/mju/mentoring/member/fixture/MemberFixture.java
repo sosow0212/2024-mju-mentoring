e8e7c0b279e788cfcc79e18307e9efe70d31f2aa
WonyuChoi <phillip07290@naver.com>
2024-02-05 20:26:58 +0900

    refactor: 상수 분리

--- src/test/java/com/mju/mentoring/member/fixture/MemberFixture.java
+++ src/test/java/com/mju/mentoring/member/fixture/MemberFixture.java
@@ -5,17 +5,20 @@ import com.mju.mentoring.member.domain.Member;
 
 public class MemberFixture {
 
+    private static final String MEMBER_DEFAULT_USERNAME = "id";
+    private static final String MEMBER_DEFAULT_NICKNAME = "nickname";
+    private static final String MEMBER_DEFAULT_PASSWORD = "password";
     private static final Long DEFAULT_ID = 1L;
 
     public static Member id_없는_멤버_생성() {
-        return Member.of("id", "password", "nickname");
+        return Member.of(MEMBER_DEFAULT_USERNAME, MEMBER_DEFAULT_PASSWORD, MEMBER_DEFAULT_NICKNAME);
     }
 
     public static Member 멤버_생성() {
         return Member.builder()
             .id(DEFAULT_ID)
-            .authInformation(AuthInformation.of("id", "password"))
-            .nickname("nickname")
+            .authInformation(AuthInformation.of(MEMBER_DEFAULT_USERNAME, MEMBER_DEFAULT_PASSWORD))
+            .nickname(MEMBER_DEFAULT_NICKNAME)
             .build();
     }
 }
