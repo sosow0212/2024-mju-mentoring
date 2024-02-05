471972452fbdf5d00b52c68cf3408bfc8875dae6
devholic22 <hyunjoon.tech@gmail.com>
2024-02-05 17:11:32 +0900

    test: AuthControllerTest 예외 추가
    - AuthControllerTest 예외 테스트 추가

--- src/test/java/com/mju/mentoring/member/controller/AuthControllerTest.java
+++ src/test/java/com/mju/mentoring/member/controller/AuthControllerTest.java
@@ -18,13 +18,17 @@ import com.mju.mentoring.member.service.dto.SignupRequest;
 import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.test.web.servlet.MockMvc;
+import java.util.stream.Stream;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
@@ -119,5 +123,105 @@ public class AuthControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.token").exists())
                 .andDo(print());
+    }
+
+    @Nested
+    class 예외_테스트 {
+
+        @ParameterizedTest(name = "닉네임이 [{0}], username이 [{1}], 비밀번호가 [{2}]인 경우")
+        @MethodSource(value = "exceptionSignupSource")
+        void 회원가입_시_빈_값이나_null은_안_된다(final String nickname, final String username, final String password)
+                throws Exception {
+            // given
+            SignupRequest request = new SignupRequest(nickname, username, password);
+            Member newMember = 회원_id_있음();
+            when(authService.signup(request)).thenReturn(newMember);
+
+            // when & then
+            mockMvc.perform(post("/auth/signup")
+                            .contentType(APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.error").exists())
+                    .andDo(print());
+        }
+
+        private static Stream<String[]> exceptionSignupSource() {
+            return Stream.of(
+                    new String[]{"", "username", "password"},
+                    new String[]{null, "username", "password"},
+                    new String[]{"nickname", "", "password"},
+                    new String[]{"nickname", null, "password"},
+                    new String[]{"nickname", "username", ""},
+                    new String[]{"nickname", "username", null},
+                    new String[]{"", "", ""},
+                    new String[]{null, null, null}
+            );
+        }
+
+        @ParameterizedTest(name = "username이 [{0}], 비밀번호가 [{1}]인 경우")
+        @MethodSource(value = "exceptionLoginSource")
+        void 쿠키_로그인_시_빈_값이나_null은_안_된다(final String nickname, final String password)
+                throws Exception {
+            // given
+            LoginRequest request = new LoginRequest(nickname, password);
+            Member newMember = 회원_id_있음();
+            when(authService.nonJwtLogin(request)).thenReturn(newMember);
+
+            // when & then
+            mockMvc.perform(post("/auth/login/cookie")
+                            .contentType(APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.error").exists())
+                    .andDo(print());
+        }
+
+        private static Stream<String[]> exceptionLoginSource() {
+            return Stream.of(
+                    new String[]{"nickname", ""},
+                    new String[]{"", "password"},
+                    new String[]{null, "password"},
+                    new String[]{"nickname", null},
+                    new String[]{"", ""},
+                    new String[]{null, null}
+            );
+        }
+
+        @ParameterizedTest(name = "username이 [{0}], 비밀번호가 [{1}]인 경우")
+        @MethodSource(value = "exceptionLoginSource")
+        void 세션_로그인_시_빈_값이나_null은_안_된다(final String nickname, final String password)
+                throws Exception {
+            // given
+            LoginRequest request = new LoginRequest(nickname, password);
+            Member newMember = 회원_id_있음();
+            when(authService.nonJwtLogin(request)).thenReturn(newMember);
+
+            // when & then
+            mockMvc.perform(post("/auth/login/session")
+                            .contentType(APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.error").exists())
+                    .andDo(print());
+        }
+
+        @ParameterizedTest(name = "username이 [{0}], 비밀번호가 [{1}]인 경우")
+        @MethodSource(value = "exceptionLoginSource")
+        void JWT_로그인_시_빈_값이나_null은_안_된다(final String nickname, final String password)
+                throws Exception {
+            // given
+            LoginRequest request = new LoginRequest(nickname, password);
+            Member newMember = 회원_id_있음();
+            when(authService.nonJwtLogin(request)).thenReturn(newMember);
+
+            // when & then
+            mockMvc.perform(post("/auth/login/jwt")
+                            .contentType(APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.error").exists())
+                    .andDo(print());
+        }
     }
 }
