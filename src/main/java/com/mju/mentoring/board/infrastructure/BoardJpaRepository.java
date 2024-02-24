3a3ec4e0018c7e5ddbf4e246ed1dfc287e922211
WonyuChoi <phillip07290@naver.com>
2024-02-24 20:28:02 +0900

    feat: 선점 잠금으로 동시성 제어

--- src/main/java/com/mju/mentoring/board/infrastructure/BoardJpaRepository.java
+++ src/main/java/com/mju/mentoring/board/infrastructure/BoardJpaRepository.java
@@ -1,14 +1,22 @@
 package com.mju.mentoring.board.infrastructure;
 
 import com.mju.mentoring.board.domain.Board;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.QueryHints;
 
 public interface BoardJpaRepository extends JpaRepository<Board, Long> {
 
     Board save(final Board board);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({
+        @QueryHint(name = "javax.persistence.lock.timeout", value = "2000")
+    })
     Optional<Board> findById(final Long id);
 
     List<Board> findAll();
