066243e056cba8ad2ba2670b4116a1617c90eaba
WonyuChoi <phillip07290@naver.com>
2024-02-28 01:20:17 +0900

    test: 닉네임 변경에 대한 테스트 작성

--- src/test/java/com/mju/mentoring/member/ui/AuthControllerWebMvcTest.java
+++ src/test/java/com/mju/mentoring/member/ui/AuthControllerWebMvcTest.java
@@ -4,6 +4,7 @@ package com.mju.mentoring.member.ui;
 import static com.mju.mentoring.global.CustomRestDocsHandler.customDocument;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willDoNothing;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
@@ -12,6 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mju.mentoring.global.BaseControllerWebMvcTest;
+import com.mju.mentoring.member.application.auth.dto.ChangeNickNameRequest;
 import com.mju.mentoring.member.application.auth.dto.SignInRequest;
 import com.mju.mentoring.member.application.auth.dto.SignupRequest;
 import com.mju.mentoring.member.ui.auth.dto.TokenResponse;
@@ -26,6 +28,10 @@ public class AuthControllerWebMvcTest extends BaseControllerWebMvcTest {
     private static final String MEMBER_DEFAULT_USERNAME = "id";
     private static final String MEMBER_DEFAULT_NICKNAME = "nickname";
     private static final String MEMBER_DEFAULT_PASSWORD = "password";
+    private static final Long MEMBER_DEFAULT_ID = 1L;
+
+    private static final String HEADER_NAME = "Authorization";
+    private static final String TOKEN_FORMAT = "Bearer accessToken...";
 
     @Autowired
     MockMvc mockMvc;
@@ -73,6 +79,24 @@ public class AuthControllerWebMvcTest extends BaseControllerWebMvcTest {
                     fieldWithPath("username").type(JsonFieldType.STRING).description("아이디"),
                     fieldWithPath("nickname").type(JsonFieldType.STRING).description("닉네임"),
                     fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호")
+                )));
+    }
+
+    @Test
+    void 닉네임_수정_테스트() throws Exception {
+        // given
+        ChangeNickNameRequest request = new ChangeNickNameRequest("new nickname");
+        willDoNothing().given(authService).changeNickName(MEMBER_DEFAULT_ID, request);
+
+        // when & then
+        mockMvc.perform(patch("/auth/nickname")
+                .header(HEADER_NAME, TOKEN_FORMAT)
+                .content(objectMapper.writeValueAsString(request))
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andDo(customDocument("change nickname",
+                requestFields(
+                    fieldWithPath("newNickname").type(JsonFieldType.STRING).description("새로운 닉네임")
                 )));
     }
 }
