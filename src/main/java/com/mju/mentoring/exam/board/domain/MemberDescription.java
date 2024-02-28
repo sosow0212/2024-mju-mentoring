1af8d05acc678b5760d200d0c1785d8d09aaa8e2
zoid79 <zidwkd00@naver.com>
2024-02-28 23:46:13 +0900

    add testcase

--- src/main/java/com/mju/mentoring/exam/board/domain/MemberDescription.java
+++ src/main/java/com/mju/mentoring/exam/board/domain/MemberDescription.java
@@ -1,6 +1,7 @@
 package com.mju.mentoring.exam.board.domain;
 
 import com.mju.mentoring.exam.board.exception.MemberNotFoundException;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
@@ -12,29 +13,29 @@ import lombok.NoArgsConstructor;
 @Embeddable
 public class MemberDescription {
 
-    @Column(nullable = false, unique = true, name = "login_id")
-    private String loginId;
+	@Column(nullable = false, unique = true, name = "login_id")
+	private String loginId;
 
-    @Column(nullable = false)
-    private String username;
+	@Column(nullable = false)
+	private String username;
 
-    @Column(nullable = false)
-    private String password;
+	@Column(nullable = false)
+	private String password;
 
-    @Column(nullable = false)
-    private String nickname;
+	@Column(nullable = false)
+	private String nickname;
 
-    public MemberDescription(String loginId, String username, String password, String nickname) {
-        this.loginId = loginId;
-        this.username = username;
-        this.password = password;
-        this.nickname = nickname;
-    }
+	public MemberDescription(String loginId, String username, String password, String nickname) {
+		this.loginId = loginId;
+		this.username = username;
+		this.password = password;
+		this.nickname = nickname;
+	}
 
-    public boolean isValidPassword(String password) {
-        if (!this.getPassword().equals(password)) {
-            throw new MemberNotFoundException("비밀번호가 일치하지 않습니다");
-        }
-        return true;
-    }
+	public boolean isValidPassword(String password) {
+		if (!this.getPassword().equals(password)) {
+			throw new MemberNotFoundException("비밀번호가 일치하지 않습니다");
+		}
+		return true;
+	}
 }
