1feb250223d089db7222112ac1e24d0708ca26ec
WonyuChoi <phillip07290@naver.com>
2024-03-03 14:17:33 +0900

    feat: 메모리로 조회수 중복 매니저 객체 구현

---
+++ src/main/java/com/mju/mentoring/board/infrastructure/MemoryViewCountManager.java
@@ -0,0 +1,40 @@
+package com.mju.mentoring.board.infrastructure;
+
+import com.mju.mentoring.board.domain.ViewCountManager;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemoryViewCountManager implements ViewCountManager {
+
+    private final Map<Long, Set<Long>> views = new HashMap<>();
+
+    @Override
+    public boolean isAlreadyRead(final Long boardId, final Long writerId) {
+        initSet(boardId);
+        return views.get(boardId)
+            .contains(writerId);
+    }
+
+    @Override
+    public void read(final Long boardId, final Long writerId) {
+        initSet(boardId);
+        views.get(boardId)
+            .add(writerId);
+    }
+
+    @Scheduled(cron = "0 0 0 * *")
+    private void clearSet() {
+        views.clear();
+    }
+
+    private void initSet(final Long boardId) {
+        if (!views.containsKey(boardId)) {
+            views.put(boardId, new HashSet<>());
+        }
+    }
+}
