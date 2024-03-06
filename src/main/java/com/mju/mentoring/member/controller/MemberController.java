eaa5f18f92ab298a6f97434653428d54831aa174
devholic22 <hyunjoon.tech@gmail.com>
2024-03-06 13:04:06 +0900

    refactor: MemberController final 속성 추가

--- src/main/java/com/mju/mentoring/member/controller/MemberController.java
+++ src/main/java/com/mju/mentoring/member/controller/MemberController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     @GetMapping("/profile")
-    public ResponseEntity<ProfileResponse> getProfile(@AuthMember Member member) {
+    public ResponseEntity<ProfileResponse> getProfile(final @AuthMember Member member) {
         ProfileResponse response = new ProfileResponse(member.getNickname(), member.getUsername());
         return ResponseEntity.ok()
                 .body(response);
